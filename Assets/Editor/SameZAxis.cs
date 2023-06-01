707548d1b047a7d9b27c03af0dcbc840884f7b20
MisterM <510249@student.saxion.nl>
2023-06-01 11:17:24 +0200

    Added tool to set all selected objects to the same global value

---
+++ Assets/Editor/SameZAxis.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SameZAxis : EditorWindow
+{
+    [MenuItem("Tools/AllignZAxis")]
+    public static void OpenTheThing() => GetWindow<SameZAxis>("Allign Z Axis");
+
+
+    public float zAxisAmount;
+
+
+    private void OnEnable()
+    {
+        Selection.selectionChanged += Repaint;
+    }
+
+    private void OnDisable()
+    {
+        Selection.selectionChanged -= Repaint;
+    }
+
+    private void OnGUI()
+    {
+
+
+        GUILayout.Label("Sets all selected objects to the same global Z-axis");
+
+        zAxisAmount = EditorGUILayout.FloatField("Global Z Axis", zAxisAmount);
+        
+
+
+       // GUILayout.Space(30);
+
+        GUILayout.Height(100);
+        using (new EditorGUI.DisabledGroupScope(Selection.gameObjects.Length == 0))
+        {
+            if (GUILayout.Button("Allign Selection ")) AllignSelection();
+        }
+
+
+    }
+
+    void AllignSelection()
+    {
+        foreach(GameObject go in Selection.gameObjects)
+        {
+            Undo.RecordObject(go.transform, "Alligned Objects on Z axis");
+
+            go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, zAxisAmount);
+        }
+    }
+}
