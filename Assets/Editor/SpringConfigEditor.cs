25e11b7e63bd6f91f2c33c03b3f8f2288ea1a7ac
MisterM <510249@student.saxion.nl>
2023-06-01 12:05:27 +0200

    Hid spring settings so only usable values are there

---
+++ Assets/Editor/SpringConfigEditor.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
+
+[CustomEditor(typeof(ZigZagSpringConfiguration))]
+public class SpringConfigEditor : Editor
+{
+
+    public void OnEnable()
+    {
+        HideOrOnHideSprings();
+    }
+
+    public void OnDisable()
+    {
+        HideOrOnHideSprings();
+    }
+
+
+    void HideOrOnHideSprings()
+    {
+
+        ZigZagSpringConfiguration script = (target as ZigZagSpringConfiguration);
+
+
+        SpringJoint[] springs = script.GetComponentsInChildren<SpringJoint>();
+
+        if (!script.HideSpringsInInspector) foreach (SpringJoint spring in springs) { spring.hideFlags = HideFlags.None; }
+        else foreach (SpringJoint spring in springs) { spring.hideFlags = HideFlags.HideInInspector; }
+
+    }
+
+
+
+
+    public void OnSceneGUI()
+    {
+        CheckStrenght();
+        CheckTolerance();
+        CheckDamper();
+
+    }
+
+
+    float oldStrenght = 0;
+
+    public void CheckStrenght()
+    {
+        ZigZagSpringConfiguration script = (ZigZagSpringConfiguration)target;
+
+        float newPercentage = script.springStrenght;
+
+
+        if (newPercentage != oldStrenght)
+        {
+
+
+            Undo.RecordObject(script, "Updated Strenght");
+            script.SetStrenghtToStrings();
+
+            EditorUtility.SetDirty(script);
+        }
+
+
+        oldStrenght = newPercentage;
+    }
+
+    float oldtolerance = 0;
+
+    public void CheckTolerance()
+    {
+        ZigZagSpringConfiguration script = (ZigZagSpringConfiguration)target;
+
+        float newPercentage = script.tolerance;
+
+
+        if (newPercentage != oldtolerance)
+        {
+
+
+            Undo.RecordObject(script, "Updated Tolerance");
+            script.SetToleranceToStrings();
+
+            EditorUtility.SetDirty(script);
+        }
+
+
+        oldtolerance = newPercentage;
+    }
+
+    float oldDamper = 0;
+
+    public void CheckDamper()
+    {
+        ZigZagSpringConfiguration script = (ZigZagSpringConfiguration)target;
+
+        float newPercentage = script.damper;
+
+
+        if (newPercentage != oldDamper)
+        {
+
+
+            Undo.RecordObject(script, "Updated Damper");
+            script.SetDamperToStrings();
+
+            EditorUtility.SetDirty(script);
+        }
+
+
+        oldDamper = newPercentage;
+    }
+}
+
--- Assets/ZigZagSpringConfiguration.cs
+++ Assets/ZigZagSpringConfiguration.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class ZigZagSpringConfiguration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Min(0)] public float springStrenght = 25;
+    [SerializeField, Min(0)] public float damper = 0.2f;
+    [SerializeField, Min(0)] public float tolerance = 0.025f;
+
+    [SerializeField] public bool HideSpringsInInspector = false;
+
+   
+
+
+    public void SetStrenghtToStrings()
     {
-        
+        SpringJoint[] springs = this.GetComponentsInChildren<SpringJoint>();
+
+        foreach(SpringJoint spring in springs)
+        {
+            spring.spring = springStrenght;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetDamperToStrings()
     {
-        
+        SpringJoint[] springs = this.GetComponentsInChildren<SpringJoint>();
+
+        foreach (SpringJoint spring in springs)
+        {
+            spring.damper = damper;
+        }
+    }
+
+    public void SetToleranceToStrings()
+    {
+        SpringJoint[] springs = this.GetComponentsInChildren<SpringJoint>();
+
+        foreach (SpringJoint spring in springs)
+        {
+            spring.tolerance = tolerance;
+        }
     }
 }
