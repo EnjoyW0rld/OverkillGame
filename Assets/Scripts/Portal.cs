bf11898513db1b31afc45e31065e046b6e1928dc
MisterM <510249@student.saxion.nl>
2023-06-05 11:05:09 +0200

    Connected Portals With Eachother

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,50 @@ public class Portal : MonoBehaviour
 {
     [SerializeField] private string bodyTag = "Body";
     [SerializeField] private Portal connectedPortal;
+    [SerializeField] private Vector2 endPoint;
 
+    private bool canTeleport = false;
+    private GameObject player;
+
+    private void Awake()
+    {
+        
+        Collider collider = GetComponent<Collider>();
+        collider.isTrigger = true;
+
+        if (connectedPortal == null) Debug.LogError("Portal: " + this.name + " doesn't have a connection. Add a connection!!!");
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && canTeleport)
+        {
+            player.transform.position = (new Vector3(connectedPortal.endPoint.x, connectedPortal.endPoint.y, 0) + connectedPortal.transform.position);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == bodyTag)
         {
-            print("beeep");
+            canTeleport = true;
+            player = other.gameObject;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == bodyTag)
+        {
+            canTeleport = false;
+
+        }
+    }
+
+
+    public Vector3 getEndPoint()
+    {
+        return endPoint;
     }
 }
