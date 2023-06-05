ba1291427783024cd521326edde122b88a9f8f40
MisterM <510249@student.saxion.nl>
2023-06-05 16:44:22 +0200

    Tried adding fisheye

--- Assets/Editor/SpringConfigEditor.cs
+++ Assets/Editor/SpringConfigEditor.cs
@@ -29,7 +29,6 @@ public class SpringConfigEditor : Editor
 
         if (!script.HideSpringsInInspector) foreach (SpringJoint spring in springs) { spring.hideFlags = HideFlags.None; }
         else foreach (SpringJoint spring in springs) { spring.hideFlags = HideFlags.NotEditable; }
-        Debug.Log(spring.hide)
 
     }
 
