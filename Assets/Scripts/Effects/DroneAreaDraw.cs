ac116d57df8c14e36d483bed2817bed1f1f1d0e1
EnjoyW0rld <denisavatar@hotmail.com>
2023-06-12 14:40:26 +0200

    Area for drone add

---
+++ Assets/Scripts/Effects/DroneAreaDraw.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class DroneAreaDraw : MonoBehaviour
+{
+    private Vector3[] vertices;
+    private int[] triangles;
+    private Mesh mesh;
+    private Vector2[] uvs;
+    [SerializeField] private Drone drone;
+    
+    private void Start()
+    {
+        drone = transform.parent.GetComponent<Drone>();
+        if (drone == null)
+        {
+            Debug.LogError("No drone in parent found");
+            return;
+        }
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        SetVertices();
+        UpdateMesh();
+    }
+
+    private void SetVertices()
+    {
+        Vector3[] points = drone.GetRayPoints();
+        vertices = new Vector3[]
+        {
+            points[0] - drone.transform.position,
+            transform.position - drone.transform.position,
+            points[1] - drone.transform.position
+        };
+        uvs = new Vector2[]
+        {
+            new Vector2(0,0),
+            new Vector2(.5f,1),
+            new Vector2(1,0)
+        };
+
+        triangles = new int[]
+        {
+            2,1,0
+        };
+    }
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        //mesh.RecalculateNormals();
+        //mesh.RecalculateUVDistributionMetrics();
+    }
+    private void OnDrawGizmos()
+    {
+        if (drone == null) return;
+        Vector3 pos = transform.position;
+        pos.y -= drone.GetRange()/2f;
+        //Gizmos.DrawWireSphere(pos, 1);
+    }
+}
