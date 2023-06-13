06194a142a4eaadaf3072e94d711db0335ff1789
MisterM <510249@student.saxion.nl>
2023-06-13 14:56:32 +0200

    Mixer

---
+++ Assets/DistortAudio.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DistortAudio : MonoBehaviour
+{
+
+    [Serializable]
+    public struct AudioEffect
+    {
+        public string name;
+        public float startValue;
+        public float endValue;
+    }
+
+    [SerializeField] List<AudioEffect> effects;
+
+    [SerializeField] AudioMixer mixer;
+
+    public void SetEffectMixer(float percentage)
+    {
+        foreach(AudioEffect effect in effects)
+        {
+            mixer.SetFloat(effect.name, Mathf.Lerp(effect.startValue, effect.endValue, percentage));   
+        }
+    }
+}
